f6b58fb9114e927736af537c67575668a97d9451
Gamze TUNÇ <tuncgamze35@gmail.com>
2024-06-20 01:05:02 +0300

    20 Haziran Autofac Kullanımı

---
+++ Business/DependencyResolvers/AutoFac/AutofacBusinessModule.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DependencyResolvers.AutoFac
+{
+    public class AutofacBusinessModule:Module   
+    {
+        protected override void Load(ContainerBuilder builder) //load ettiğinde yani uygulama çalıştığında override etcek içeridekilerini çalıştırcak
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance(); //bir tane örnek oluşturur sadece çağırmalar için kullanırız data saklamayız içinde 
+            //builder.RegisterType<ProductManager>().As<IProductService>(); // IProductService istendiğinde ProductManager new le yani örneği instance yi ver
+            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+        }
+    }
+}
